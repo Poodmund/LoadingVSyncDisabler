e239f70b8702bd90e99c66ef5a45841dd946bb53
Poodmund <eddieturner2002@gmail.com>
2021-07-11 12:28:59 +0100

    Commit project

---
+++ LoadingVSyncDisabler/DisableLoadingVSync.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+// Lightweight Kerbal Space Program plugin to ensure that VSync is disabled during the loading screen, code & concept shameless copied from Gameslinx's Parallax mod.
+
+namespace LoadingVSyncDisabler
+{
+    [KSPAddon((KSPAddon.Startup)(-2), false)]
+	public class DisableLoadingVSync : MonoBehaviour
+	{
+		public void Start()
+		{
+			QualitySettings.vSyncCount = 0;
+		}
+	}
+}
